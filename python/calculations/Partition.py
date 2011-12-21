7e3048468630528ad41c9cfb1e3db1dd17cdda9b
Daniel Lytkin <danlytkin@daniel.(none)>
2011-12-21 13:39:06 +0700

    changed folder structure, added partitions iterator, some algorithm fixes + tests

---
+++ python/calculations/Partition.py
@@ -0,0 +1,100 @@
+__author__ = 'Daniel Lytkin'
+
+
+class Partitions:
+    r"""
+    Class providing iterator on partitions and size method
+    """
+    def __init__(self, arg):
+        self.number = arg
+
+    def __iter__(self):
+        self.current = Partition(self.number)
+        return self
+
+    def next(self):
+        nextPartition = self.current.next()
+        self.current = nextPartition
+        return self.current
+
+    def size(self):
+        r"""
+        Returns number of partitions
+        """
+        # TODO
+        pass
+
+
+class Partition(list):
+    r"""
+    This class provides methods to generate partitions of natural numbers.
+    """
+    def __init__(self, arg):
+        r"""
+        When called with integer argument n, creates zero partition, which is []. By calling next() you'll get [5].
+        If the argument is list, it is treated as partition
+        """
+        if isinstance(arg, long) or isinstance(arg, int):
+            self.number = arg
+            self._h = 1  # number of elements distinct from 1
+            super(Partition, self).__init__()
+        elif isinstance(arg, Partition):
+            super(Partition, self).__init__(arg)
+            self._h = arg._h
+        else:
+            # self.number = sum(arg) # possibly redundant
+            super(Partition, self).__init__(arg)
+            self._h = len(filter(lambda x: x>1, self))
+
+    def transpose(self):
+        r"""
+        Transposes the partition, e. g. its Ferrers diagram.
+        partition must be in descending order
+        """
+        transposed = []
+        i = 0
+        while True:
+            counter = len(filter(lambda x: x-i>0, self))
+            if counter:
+                transposed.append(counter)
+                i += 1
+            else:
+                break
+        return Partition(transposed)
+
+
+    def next(self):
+        if not len(self):
+            return Partition([self.number])
+
+
+        if self[0] == 1:
+            raise StopIteration
+
+        x = Partition(self)
+        h = x._h
+        if x[h-1] == 2:
+            x[h-1]=1
+            x.append(1)
+            h -= 1
+        else:
+            r = x[h-1]-1   # the last element which is greater then 1 minus 1
+            t = len(x)-h+1   # number of ones + 1
+            x[h-1] = r
+            while t >= r:
+                h += 1
+                x[h-1] = r
+                t -= r
+            if not t:
+                del x[h:]  # remove redundant ones
+            else:
+                if h+1 <= len(x):
+                    del x[h+1:]
+                else:
+                    x[len(x):h+1] = [1]*(h+1-len(x))
+                if t > 1:
+                    h += 1
+                    x[h-1] = t
+        x._h = h
+        return x
+
--- test/PartitionsTest.py
+++ test/PartitionsTest.py
@@ -1,16 +1,38 @@
-from Partition import Partition
 import unittest
+from calculations.Partition import *
 
 __author__ = 'Daniel Lytkin'
 
 class PartitionsTest(unittest.TestCase):
     def test_transpose(self):
-        partition = [8, 8, 6,5,3,3,3,1]
-        transposed = Partition.transpose(partition)
+        partition = Partition([8, 8, 6,5,3,3,3,1])
+        transposed = partition.transpose()
         self.assertEqual(sum(partition), sum(transposed))
         self.assertEqual(transposed, [8, 7, 7, 4, 4, 3, 2, 2])
 
+    def test_next(self):
+        allPartitions = []
+        for p in Partitions(10):
+            allPartitions.append(p)
+
+        expected = [[10], [9, 1], [8, 2], [8, 1, 1], [7, 3],
+            [7, 2, 1], [7, 1, 1, 1], [6, 4], [6, 3, 1],
+            [6, 2, 2], [6, 2, 1, 1], [6, 1, 1, 1, 1],
+            [5, 5], [5, 4, 1], [5, 3, 2], [5, 3, 1, 1],
+            [5, 2, 2, 1], [5, 2, 1, 1, 1], [5, 1, 1, 1, 1, 1],
+            [4, 4, 2], [4, 4, 1, 1], [4, 3, 3], [4, 3, 2, 1],
+            [4, 3, 1, 1, 1], [4, 2, 2, 2], [4, 2, 2, 1, 1],
+            [4, 2, 1, 1, 1, 1], [4, 1, 1, 1, 1, 1, 1],
+            [3, 3, 3, 1], [3, 3, 2, 2], [3, 3, 2, 1, 1],
+            [3, 3, 1, 1, 1, 1], [3, 2, 2, 2, 1],
+            [3, 2, 2, 1, 1, 1], [3, 2, 1, 1, 1, 1, 1],
+            [3, 1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 1, 1], [2, 2, 2, 1, 1, 1, 1],
+            [2, 2, 1, 1, 1, 1, 1, 1], [2, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+        self.assertSequenceEqual(expected, allPartitions)
 
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
