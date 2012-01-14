9502826683f9904c57610cfcad3867c67f9b202a
Daniel Lytkin <gaelco.salmon@gmail.com>
2012-01-14 19:20:04 +0700

    removed BoundedSet class, plus some algorithm optimizations

--- src/calculations/set.py
+++ src/calculations/set.py
@@ -1,84 +1,78 @@
 __author__ = 'Daniel Lytkin'
 
 
-#def generateSets(bound, start=None, maximal=False):
-#    if start is None:
-#        start = bound
-#    if not bound or not start: return [[]]
-#    sets = []
-#    for i in range(start,0, -1):
-#        sets += [[i] + s for s in generateSets(bound-i, min(bound-i,i-1), maximal)]
-#    return sets + [[]]*(not maximal)
-
-
 
 class BoundedSets:
-    r"""
-    Generates integer sets {n_1,...,n_k}, such that n_1+...+n_k <= n
+    """Generates integer sets {n_1,...,n_k}, such that n_1+...+n_k <= n.
+    If optional parameter 'maximal' is set to True, then generates only those sets that
+    are maximal under containment relation.
     """
+    @staticmethod
+    def _next(sequence, bound, sum):
+        if sequence[0] <= 1:
+            return None, None
+
+        x = list(sequence)
+        sum -= 1
+        if x[-1] == 1: # if the last element is 1, remove it
+            del x[-1]
+        else:
+            x[-1] -= 1
+            while True:
+                t = min(x[-1]-1, bound - sum)
+                if not t: break
+                sum += t
+                x.append(t)
+        return x, sum
+
+    @staticmethod
+    def next(sequence, bound):
+        """Returns next bounded set after sequence in reversed lexicographical order
+        """
+        return BoundedSets._next(sequence, bound, sum(sequence))[0]
+
+    @staticmethod
+    def _nextMaximal(sequence, bound, sum):
+        if sequence[0] <= 1:
+            return None, None
+
+        x = list(sequence)
+        try:
+            i = 1
+            while x[-1] == i: # if tail is [N, i, i-1, i-2, ..., 1], remove it ant set to [N-1]
+                i+=1
+                sum -= x[-1]
+                del x[-1]
+        except IndexError:
+        # this means that sequence is [i, i-1, i-2, ..., 1] thus it is the last possible maximal set
+            return None, None
+
+        x[-1] -= 1
+        sum -= 1
+        while True:
+            t = min(x[-1]-1, bound - sum)
+            if not t: break
+            sum += t
+            x.append(t)
+        return x, sum
+
+    @staticmethod
+    def nextMaximal(sequence, bound):
+        """Returns next maximal bounded set after sequence in reversed lexicographical order.
+        Input sequence itself must be maximal, otherwise output will be incorrect.
+        """
+        return BoundedSets._nextMaximal(sequence, bound, sum(sequence))[0]
+
     def __init__(self, bound, maximal=False):
         self._bound = bound
         self._maximal = maximal
 
     def __iter__(self):
-        current = BoundedSet(self._bound, maximal=self._maximal)
+        current = [self._bound]
+        sum = self._bound
+        nextSet = BoundedSets._nextMaximal if self._maximal else BoundedSets._next
         while current is not None:
             yield current
-            current = current.next()
+            current, sum = nextSet(current, self._bound, sum)
 
 
-class BoundedSet(list):
-    # TODO: doc
-    def __init__(self, *args, **kwargs):
-        if isinstance(args[0], list):
-            super(BoundedSet, self).__init__(args[0])
-        else:
-            super(BoundedSet, self).__init__(args)
-        self._bound, self._sum = (args[0]._bound, args[0]._sum) \
-        if isinstance(args[0], BoundedSet) else [sum(self)]*2
-
-        self._maximal = kwargs.get("maximal") or False
-        self.next = self._nextMax if self._maximal else self._next
-
-    def _next(self): # generate next set
-        if self[0] <= 1:
-            return None
-
-        x = BoundedSet(self)
-        if x[-1] == 1: # if the last element is 1, remove it
-            x._sum -= 1
-            del x[-1]
-        else:
-            x[-1] -= 1
-            x._sum -= 1
-            while True:
-                t = min(x[-1]-1, x._bound - x._sum)
-                if not t: break
-                x._sum += t
-                x.append(t)
-        return x
-
-    def _nextMax(self):
-        if self[0] == 1:
-            return None
-
-        x = BoundedSet(self, maximal=True)
-
-        try:
-            i = 1
-            while x[-1] == i: # if tail is [k, i, i-1, i-2, ..., 1], remove it ant set to [k-1]
-                i+=1
-                x._sum -= x[-1]
-                del x[-1]
-        except IndexError:
-            return None
-
-        x[-1] -= 1
-        x._sum -= 1
-        while True:
-            t = min(x[-1]-1, x._bound - x._sum)
-            if not t: break
-            x._sum += t
-            x.append(t)
-
-        return x
--- test/calculations/set_test.py
+++ test/calculations/set_test.py
@@ -12,6 +12,14 @@ class SetTest(unittest.TestCase):
         """
         return filter(lambda x: not any(set(x)<set(y) for y in sets), sets)
 
+    def test_next_bounded_set(self):
+        expected = [4, 3, 2, 1]
+        self.assertSequenceEqual(expected, BoundedSets.next([5], 10))
+
+    def test_next_maximal_set(self):
+        expected = [4, 3, 2, 1]
+        self.assertSequenceEqual(expected, BoundedSets.nextMaximal([5, 2, 1], 10))
+
     def test_sets(self):
         sets = list(BoundedSets(10))
         expected = [[10], [9, 1], [9], [8, 2], [8, 1], [8], [7, 3], [7, 2, 1],
@@ -26,7 +34,7 @@ class SetTest(unittest.TestCase):
         """
         Maximal sets are exactly the sets that are not contained in any other
         """
-        n = 30
+        n = 20
         sets = list(BoundedSets(n, maximal=True))
         expected = self.filterContained(list(BoundedSets(n)))
         self.assertSequenceEqual(expected, sets)
@@ -36,7 +44,7 @@ class SetTest(unittest.TestCase):
         Every partition must have the set of its parts contained in some bounded set for the same n.
         We only need sets that are maximal by containment
         """
-        n = 30
+        n = 20
         minusPartitions = list(BoundedSets(n, maximal=True))
         allPartitions = list(Partitions(n))
 
