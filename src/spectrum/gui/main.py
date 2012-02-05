3d02641aced4ca1f3593d68a2101d866da3cdf66
Daniel Lytkin <gaelco.salmon@gmail.com>
2012-02-05 12:28:56 +0700

    added full graphs and mouse selection

--- src/spectrum/gui/main.py
+++ src/spectrum/gui/main.py
@@ -1,5 +1,5 @@
 from spectrum.graph.drawing import GraphViewer
-from spectrum.graph.graph import Graph
+from spectrum.graph.graph import Graph, FullGraph
 from spectrum.graph.layout import RandomLayout
 
 __author__ = 'Daniel Lytkin'
@@ -10,9 +10,9 @@ root = Tk()
 frame = Frame(master=root)
 frame.pack()
 
-graph = Graph()
-graph.add_vertices(range(10))
-graph.add_edge(0, 1)
+graph = FullGraph(5)
+#graph.add_vertices(range(10))
+#graph.add_edge(0, 1)
 
 canvas = GraphViewer(RandomLayout(graph))
 canvas.pack()
