a331064f9174081d3fa91d337a24c6fa0969cb3e
Dan Lytkin <d.lytkin@s7.ru>
2012-04-10 13:31:30 +0700

    added iterations plugin, added vertex labels

--- src/spectrum/gui/graph/graph_canvas.py
+++ src/spectrum/gui/graph/graph_canvas.py
@@ -52,20 +52,20 @@ class MousePlugin(object):
     """Class responsible for all mouse manipulations with graph on canvas.
     """
 
-    def __init__(self, container):
-        """Initializes the plugin for 'container' canvas
+    def apply(self, canvas):
+        """Initializes the plugin for given canvas
         """
-        self._container = container
+        self._canvas = canvas
 
         self._selection = None
         self._selection_rect = None
         self._click = None
         self._click_id = None
-        self._picked_state = container.picked_vertex_state
+        self._picked_state = canvas.picked_vertex_state
 
-        container.bind("<Button-1>", self._on_press)
-        container.bind("<ButtonRelease-1>", lambda event: self._on_release)
-        container.bind("<Motion>", self._on_drag)
+        canvas.bind("<Button-1>", self._on_press)
+        canvas.bind("<ButtonRelease-1>", self._on_release)
+        canvas.bind("<Motion>", self._on_drag)
 
     def _on_drag(self, event):
         if self._click is not None:
@@ -73,21 +73,21 @@ class MousePlugin(object):
                 picked = self._picked_state.get_picked()
                 if picked:
                     for vertex in picked:
-                        self._container.move_vertex(vertex,
+                        self._canvas.move_vertex(vertex,
                             Point(event.x, event.y) - self._click)
                     self._click = Point(event.x, event.y)
             else:
                 x0, x1 = ordered_pair(self._selection.x, event.x)
                 y0, y1 = ordered_pair(self._selection.y, event.y)
-                self._container.coords(self._selection_rect, x0, y0, x1, y1)
+                self._canvas.coords(self._selection_rect, x0, y0, x1, y1)
 
     def _on_press(self, event):
         self._click = Point(event.x, event.y)
         self._click_id = None
-        id = self._container.get_object_id_by_location(self._click)
-        if self._container.is_vertex(id):
+        id = self._canvas.get_object_id_by_location(self._click)
+        if self._canvas.is_vertex(id):
             self._click_id = id
-            vertex = self._container._get_vertex_by_shape_id(id)
+            vertex = self._canvas._get_vertex_by_shape_id(id)
             if (not self._picked_state.is_picked(vertex) and
                 not event.state & 1):
                 self._picked_state.clear()
@@ -96,28 +96,57 @@ class MousePlugin(object):
             if not event.state & 1:
                 self._picked_state.clear()
             self._selection = Point(event.x, event.y)
-            self._selection_rect = self._container.create_rectangle(
+            self._selection_rect = self._canvas.create_rectangle(
                 event.x, event.y, event.x, event.y, **SELECTION_KW)
 
-    def _on_release(self):
+    def _on_release(self, event):
         self._click = None
         if self._selection is not None:
-            selection = self._container.coords(self._selection_rect)
-            for id in self._container.find_overlapping(*selection):
-                if self._container.is_vertex(id):
-                    vertex = self._container._get_vertex_by_shape_id(id)
+            selection = self._canvas.coords(self._selection_rect)
+            for id in self._canvas.find_overlapping(*selection):
+                if self._canvas.is_vertex(id):
+                    vertex = self._canvas._get_vertex_by_shape_id(id)
                     self._picked_state.pick(vertex)
-            self._container.delete(self._selection_rect)
+            self._canvas.delete(self._selection_rect)
             self._selection = None
+
+
+class IterationsPlugin(object):
+    """This plugin enables animation for iterable graph layouts
+    """
+
+    def apply(self, canvas, time_step=50):
+        """Applies plugin for given canvas. Default FPS is 30
+        """
+        self._canvas = canvas
+        self._event_id = None
+        self.time_step = time_step
+
+    def _step(self):
+        self._canvas.layout.step()
+        self._canvas.reset() # TODO: temp
+        self._event_id = self._canvas.after(self.time_step, self._step)
+
+    def iterate(self, times):
+        for i in xrange(times):
+            self._canvas.layout.step()
+        self._canvas.reset()
+
+    def start(self):
+        self._step()
+
+    def stop(self):
+        self._canvas.after_cancel(self._event_id)
 
 
 class Vertex(object):
     """This class connects graph vertex with its shape on the canvas.
     """
 
-    def __init__(self, value, shape):
+    def __init__(self, value, shape, label):
         self._value = value
         self._shape = shape
+        self._label = label
         self._shape.add_tag("vertex")
         self._incident = set()
 
@@ -126,6 +155,12 @@ class Vertex(object):
         """Returns the shape that corresponds to this vertex
         """
         return self._shape
+
+    @property
+    def label_id(self):
+        """Returns 'text' canvas element, that displays vertex value
+        """
+        return self._label
 
     @property
     def value(self):
@@ -184,7 +219,7 @@ class GraphCanvas(Canvas):
 
         # self._translate = (0, 0)
 
-        self._mouse_plugin = MousePlugin(self)
+        self._mouse_plugin = MousePlugin().apply(self)
 
         # makes vertex shape change appearing if it was picked
         # also sets layout lock for picked vertices
@@ -242,13 +277,16 @@ class GraphCanvas(Canvas):
             self.delete(id)
 
     def _add_vertex(self, value):
-        new_vertex = Vertex(value, self._create_vertex_shape(value))
+        label = self.create_text(0, 0, text=value)
+        new_vertex = Vertex(value, self._create_vertex_shape(value), label)
 
         layout_location = self._layout[self.graph.index(value)]
         canvas_location = self._convert_layout_location(layout_location)
         self.set_vertex_location(new_vertex, canvas_location)
+
         self._vertices[value] = new_vertex
 
+        # this is for variable vertex shape sizes
         self.__margin = max(self.__margin, new_vertex.shape.radius)
 
     def _add_edge(self, start_index, end_index):
@@ -280,11 +318,13 @@ class GraphCanvas(Canvas):
 
     def reset(self):
         # TODO: temporary
-        """Reset vertex positions."""
+        """Reset vertex positions on canvas to layout coordinates"""
         for value in self.graph.vertices:
             layout_location = self.layout[self.graph.index(value)]
             canvas_location = self._convert_layout_location(layout_location)
             self.set_vertex_location(self._vertices[value], canvas_location)
+            location = self.coords(self._vertices[value].shape.id)
+            #self.coords(self._vertices[value].label_id, *location)
 
     def is_vertex(self, id):
         """Checks whether given shape id represents a vertex."""
@@ -295,6 +335,7 @@ class GraphCanvas(Canvas):
         """
         id = vertex.shape.id
         self.move(id, v.x, v.y)
+        self._update_vertex_label_location(vertex)
         canvas_location = self._get_shape_center(id)
         layout_location = self._convert_canvas_location(canvas_location)
         self._layout[self.graph.index(vertex.value)] = layout_location
@@ -304,6 +345,12 @@ class GraphCanvas(Canvas):
                 self._move_edge(edge, v, Point())
             else:
                 self._move_edge(edge, Point(), v)
+
+    def _update_vertex_label_location(self, vertex):
+        v_location = self.coords(vertex.shape.id)
+        self.coords(vertex.label_id, *v_location[:2])
+        self.tag_raise(vertex.label_id, vertex.shape.id)
+
 
     def _get_shape_center(self, id):
         x, y, x1, y1 = self.coords(id)
