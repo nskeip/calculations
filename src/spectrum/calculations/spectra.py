f416eb0e04946027899efcc51927f91ae198c7a7
Daniel Lytkin <dan.lytkin@gmail.com>
2013-10-17 13:20:57 +0700

    added bibliography for linear, unitary, symplectic and orthogonal groups spectra results; reformatted code

--- src/spectrum/calculations/spectra.py
+++ src/spectrum/calculations/spectra.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
 """
+Bibliography:
+[1] Buturlakin A. A., “Spectra of finite symplectic and orthogonal groups”, Siberian Adv. in Math., 21, No. 3, 176–210
+    (2011).
+[2] Buturlakin A. A., “Spectra of finite linear and unitary groups”, Algebra and Logic, Vol. 47, No. 2, (2008).
+
+
 Copyright 2012 Daniel Lytkin.
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,11 +43,10 @@ sporadic_spectra = {
     "2F4(2)'": (10, 12, 13, 16),
     'Fi23': (16, 17, 22, 23, 24, 26, 27, 28, 35, 36, 39, 42, 60),
     'He': (8, 10, 12, 15, 17, 21, 28),
-    'B': (25, 27, 31, 32, 34, 36, 38, 39, 40, 42, 44, 46, 47, 48, 52, 55, 56,
-          60, 66, 70),
+    'B': (25, 27, 31, 32, 34, 36, 38, 39, 40, 42, 44, 46, 47, 48, 52, 55, 56, 60, 66, 70),
     'M24': (8, 10, 11, 12, 14, 15, 21, 23),
-    'M': (32, 36, 38, 40, 41, 45, 48, 50, 51, 54, 56, 57, 59, 60, 62, 66, 68,
-          69, 70, 71, 78, 84, 87, 88, 92, 93, 94, 95, 104, 105, 110, 119),
+    'M': (32, 36, 38, 40, 41, 45, 48, 50, 51, 54, 56, 57, 59, 60, 62, 66, 68, 69, 70, 71, 78, 84, 87, 88, 92, 93, 94,
+          95, 104, 105, 110, 119),
     'M22': (5, 6, 7, 8, 11),
     'M23': (6, 8, 11, 14, 15, 23),
     "O'N": (11, 12, 15, 16, 19, 20, 28, 31),
@@ -55,22 +61,25 @@ sporadic_spectra = {
 }
 
 
-
-
 #############################
 # SPECTRA OF CLASSICAL GROUPS
 #############################
 def _symplectic_spectrum_odd_c(n, field):
-    """Spectra of symplectic groups in odd characteristic
+    """Spectra of symplectic groups in odd characteristic.
+    [1, Corollary 1]
     """
     n //= 2
     q = field.order
     p = field.char
+
     # (1)
     a1 = SemisimpleElements(q, n)
+
     # (2)
-    a2 = MixedElements(q, n, lambda k: (p ** (k - 1) + 1) // 2,
+    a2 = MixedElements(q, n,
+                       lambda k: (p ** (k - 1) + 1) // 2,
                        lambda k: p ** k)
+
     # (3)
     k = numeric.get_exponent(2 * n - 1, p)
     a3 = [] if k is None else [2 * p * (2 * n - 1)]
@@ -78,17 +87,23 @@ def _symplectic_spectrum_odd_c(n, field):
 
 
 def _symplectic_spectrum_even_c(n, field):
-    """Spectra of symplectic groups in characteristic 2
+    """Spectra of symplectic groups in characteristic 2.
+    [2, Corollary 3]
     """
     n //= 2
     q = field.order
+
     # (1)
     a1 = SemisimpleElements(q, n)
+
     # (2)
     a2 = (2 * elem for elem in SemisimpleElements(q, n - 1))
+
     # (3)
-    a3 = MixedElements(q, n, lambda k: 2 ** (k - 1) + 1,
+    a3 = MixedElements(q, n,
+                       lambda k: 2 ** (k - 1) + 1,
                        lambda k: 2 ** (k + 1))
+
     # (4)
     k = numeric.get_exponent(n - 1, 2)
     a4 = [] if k is None else [(n - 1) * 4]
@@ -105,19 +120,24 @@ def _symplectic_spectrum(n, field):
 
 
 def _projective_symplectic_spectrum_odd_c(n, field):
-    """Spectra of projective symplectic groups in characteristic 2
+    """Spectra of projective symplectic groups in characteristic 2.
+    [1, Corollary 2]
     """
     n //= 2
     q = field.order
     p = field.char
+
     # (1)
     t = (q ** n - 1) // 2
     a1 = [t, t + 1]
+
     # (2)
     a2 = SemisimpleElements(q, n, min_length=2)
+
     # (3)
     a3 = MixedElements(q, n, lambda k: (p ** (k - 1) + 1) // 2,
                        lambda k: p ** k)
+
     # (4)
     k = numeric.get_exponent(2 * n - 1, p)
     a4 = [] if k is None else [p * (2 * n - 1)]
@@ -135,26 +155,36 @@ def _projective_symplectic_spectrum(n, field):
 
 
 def _omega_spectrum_odd_c(n, field):
+    """Spectra of groups \Omega_{2n+1}(q) for odd q.
+    [1, Corollary 6]
+    """
     n = (n - 1) // 2
     q = field.order
     p = field.char
+
     # (1)
     t = (q ** n - 1) // 2
     a1 = [t, t + 1]
+
     # (2)
     a2 = SemisimpleElements(q, n, min_length=2)
+
     # (3)
     k = 1
     a3 = []
     while True:
         n1 = n - (p ** (k - 1) + 1) // 2
-        if n1 < 1: break
+        if n1 < 1:
+            break
         t = (q ** n1 - 1) // 2
         a3.extend([t * p ** k, (t + 1) * p ** k])
         k += 1
-        # (4)
-    a4 = MixedElements(q, n, lambda k: (p ** (k - 1) + 1) // 2,
+
+    # (4)
+    a4 = MixedElements(q, n,
+                       lambda k: (p ** (k - 1) + 1) // 2,
                        lambda k: p ** k, min_length=2)
+
     # (5)
     k = numeric.get_exponent(2 * n - 1, p)
     a5 = [] if k is None else [p * (2 * n - 1)]
@@ -173,26 +203,35 @@ def _omega_spectrum(n, field):
 
 
 def _omega_pm_spectrum_odd_c(n, field, sign):
+    """Spectra of Omega^e_{2n}(q) for odd q.
+    [1, Corollary 8]
+    """
     n //= 2
     q = field.order
     p = field.char
     nk = lambda k: (p ** (k - 1) + 3) // 2
+
     # (1)
     a1 = [(q ** n - sign) // 2]
+
     # (2)
     a2 = SemisimpleElements(q, n, min_length=2, parity=sign)
+
     # (3)
     a3 = []
     k = 1
     while True:
         n_k = nk(k)
-        if n_k >= n: break
+        if n_k >= n:
+            break
         dk = gcd(4, q ** n_k - sign) // 2
         a3.append(p ** k * lcm(dk, (q ** (n - n_k) + 1) // dk))
         a3.append(p ** k * lcm(dk, (q ** (n - n_k) - 1) // dk))
         k += 1
-        # (4)
+
+    # (4)
     a4 = MixedElements(q, n, nk, lambda k: p ** k, min_length=2)
+
     # (5)
     a5 = []
     for elem in SemisimpleElements(q, n - 2, min_length=2, parity=sign):
@@ -202,42 +241,55 @@ def _omega_pm_spectrum_odd_c(n, field, sign):
     # (6)
     t = (q ** (n - 2) - sign) // 2
     a6 = [p * lcm(q - 1, t), p * lcm(q + 1, t)]
+
     # (7)
     k = numeric.get_exponent(2 * n - 3, p)
     a7 = [] if k is None else [p * (2 * n - 3) * gcd(4, q ** n - sign) // 2]
+
     # (8)
     a8 = [p * (q * q - 1), p * (q * q + 1)] if n == 4 and sign == 1 else []
+
     # (9)
     a9 = [9 * (q - 1), 9 * (q + 1)] if n == 4 and p == 3 and sign == 1 else []
     return itertools.chain(a1, a2, a3, a4, a5, a6, a7, a8, a9)
 
 
 def _omega_pm_spectrum_even_c(n, field, sign):
+    """Spectra for groups \Omega^{\pm}(2^k).
+    [1, Corollary 4]
+    """
     n //= 2
     q = field.order
+
     # (1)
     a1 = SemisimpleElements(q, n, parity=sign)
+
     # (2)
-    a2 = MixedElements(q, n, lambda k: 2 ** (k - 1) + 2,
+    a2 = MixedElements(q, n,
+                       lambda k: 2 ** (k - 1) + 2,
                        lambda k: 2 ** (k + 1))
+
     # (3)
     a3 = (2 * elem for elem in SemisimpleElements(q, n - 2))
+
     # (4)
     a4 = []
     for elem in SemisimpleElements(q, n - 2, parity=sign):
         a4.append(2 * lcm(q - 1, elem))
         a4.append(2 * lcm(q + 1, elem))
-        # (5)
+
+    # (5)
     a5 = []
     signMod = 0 if sign == 1 else 1
     for ni in FullBoundedSets(n - 3):
-        if len(ni) % 2 != signMod: continue
+        if len(ni) % 2 != signMod:
+            continue
         a5.append(4 * SpectraElement(q=q, partition=[1] + ni,
                                      signs=[-1] + [1] * len(ni)))
-        # (6)
-    a6 = (elem.lcm(
-        SpectraElement(4, q, [1], [1])) for elem in SemisimpleElements(q, n - 3
-        , parity=-sign))
+
+    # (6)
+    a6 = (elem.lcm(SpectraElement(4, q, [1], [1])) for elem in SemisimpleElements(q, n - 3, parity=-sign))
+
     # (7)
     k = numeric.get_exponent(n - 2, 2)
     a7 = [] if k is None else [4 * (n - 2)]
@@ -253,6 +305,9 @@ def _equal_two_part(a, b):
 
 
 def _projective_omega_pm_spectrum(sign):
+    """Spectra of Omega^e_{2n}(q) for odd q.
+    [1, Corollary 9]
+    """
     e = sign
 
     def spectrum(n, field):
@@ -265,8 +320,10 @@ def _projective_omega_pm_spectrum(sign):
             return _omega_pm_spectrum(e)(n * 2, field)
 
         nk = lambda k: (p ** (k - 1) + 3) // 2
+
         # (1)
         a1 = [(q ** n - sign) // 4]
+
         # (2)
         a2 = []
         for n1 in xrange(1, n):
@@ -275,19 +332,24 @@ def _projective_omega_pm_spectrum(sign):
                 b = q ** (n - n1) - e * e1
                 d = 2 if _equal_two_part(a, b) else 1
                 a2.append(lcm(a, b) // d)
-                # (3)
+
+        # (3)
         a3 = SemisimpleElements(q, n, min_length=3, parity=sign)
+
         # (4)
         a4 = []
         k = 1
         while True:
             n_k = nk(k)
-            if n_k >= n: break
+            if n_k >= n:
+                break
             a4.append(p ** k * (q ** (n - n_k) + 1) // 2)
             a4.append(p ** k * (q ** (n - n_k) - 1) // 2)
             k += 1
-            # (5)
+
+        # (5)
         a5 = MixedElements(q, n, nk, lambda k: p ** k, min_length=2)
+
         # (6)
         a6 = []
         for elem in SemisimpleElements(q, n - 2, min_length=2, parity=sign):
@@ -297,6 +359,7 @@ def _projective_omega_pm_spectrum(sign):
         # (7)
         t = (q ** (n - 2) - sign) // 2
         a7 = [p * lcm(q - 1, t), p * lcm(q + 1, t)]
+
         # (8)
         k = numeric.get_exponent(2 * n - 3, p)
         a8 = [] if k is None else [p * (2 * n - 3)]
@@ -318,14 +381,21 @@ def _omega_pm_spectrum(sign):
 
 
 def _special_orthogonal_odd_c_spectrum(n, field):
+    """Spectra of groups SO_{2n+1}(q) for odd q.
+    [1, Corollary 5]
+    """
     n = (n - 1) // 2
     q = field.order
     p = field.char
+
     # (1)
     a1 = SemisimpleElements(q, n)
+
     # (2)
-    a2 = MixedElements(q, n, lambda k: (p ** (k - 1) + 1) // 2,
+    a2 = MixedElements(q, n,
+                       lambda k: (p ** (k - 1) + 1) // 2,
                        lambda k: p ** k)
+
     # (3)
     k = numeric.get_exponent(2 * n - 1, p)
     a3 = [] if k is None else [p * (2 * n - 1)]
@@ -333,23 +403,31 @@ def _special_orthogonal_odd_c_spectrum(n, field):
 
 
 def _special_orthogonal_pm_spectrum(sign):
+    """Spectra SO^e_{2n}(q) for odd q.
+    [1, Corollary 7]
+    """
     e = sign
 
     def spectrum(n, field):
         n //= 2
         q = field.order
         p = field.char
+
         # (1)
         a1 = SemisimpleElements(q, n, parity=e)
+
         # (2)
-        a2 = MixedElements(q, n, lambda k: (p ** (k - 1) + 3) // 2,
+        a2 = MixedElements(q, n,
+                           lambda k: (p ** (k - 1) + 3) // 2,
                            lambda k: p ** k)
+
         # (3)
         a3 = []
         for elem in SemisimpleElements(q, n - 2, parity=e):
             a3.append(elem.lcm(SpectraElement(p, q, [1], [-1])))
             a3.append(elem.lcm(SpectraElement(p, q, [1], [1])))
-            # (4)
+
+        # (4)
         k = numeric.get_exponent(2 * n - 3, p)
         a4 = [] if k is None else [2 * p * (2 * n - 3)]
         return itertools.chain(a1, a2, a3, a4)
@@ -359,19 +437,27 @@ def _special_orthogonal_pm_spectrum(sign):
 
 # PGL and PGU
 def _projective_general_linear_spectrum(sign):
+    """Spectra of PGL and PGU groups.
+    [2, Corollary 2]
+    """
     e = sign
 
     def spectrum(n, field):
         q = field.order
         p = field.char
+
         # (1)
         eps = 1 if n % 2 == 0 else e
         a1 = [(q ** n - eps) // (q - e)]
+
         # (2)
         a2 = SemisimpleElements(q, n, min_length=2, sign=e)
+
         # (3)
-        a3 = MixedElements(q, n, lambda k: p ** (k - 1) + 1,
+        a3 = MixedElements(q, n,
+                           lambda k: p ** (k - 1) + 1,
                            lambda k: p ** k, sign=e)
+
         # (4)
         k = numeric.get_exponent(n - 1, p)
         a4 = [] if k is None else [p * (n - 1)]
@@ -381,16 +467,22 @@ def _projective_general_linear_spectrum(sign):
 
 
 def _special_linear_spectrum(sign):
+    """Spectra of SL and SU groups.
+        [2, Corollary 1]
+    """
     e = sign
 
     def spectrum(n, field):
         q = field.order
         p = field.char
+
         # (1)
         eps = 1 if n % 2 == 0 else e
         a1 = [(q ** n - eps) // (q - e)]
+
         # (2)
         a2 = SemisimpleElements(q, n, min_length=2, sign=e)
+
         # (3)
         a3 = []
         k = 1
@@ -402,12 +494,16 @@ def _special_linear_spectrum(sign):
             eps = 1 if n1 % 2 == 0 else e
             a3.append(p ** k * (q ** n1 - eps) / gcd(d, n1))
             k += 1
-            # (4)
-        a4 = MixedElements(q, n, lambda k: p ** (k - 1) + 1,
+
+        # (4)
+        a4 = MixedElements(q, n,
+                           lambda k: p ** (k - 1) + 1,
                            lambda k: p ** k, min_length=2, sign=e)
+
         # (5)
         k = numeric.get_exponent(n - 1, p)
         a5 = [] if k is None else [p * (n - 1) * d]
+
         # (6)
         a6 = [p * gcd(2, q - 1) * (q + e)] if n == 4 else []
         return itertools.chain(a1, a2, a3, a4, a5, a6)
@@ -416,15 +512,20 @@ def _special_linear_spectrum(sign):
 
 
 def _projective_special_linear_spectrum(sign):
+    """Spectra of PSL and PSU groups.
+        [2, Corollary 3]
+    """
     e = sign
 
     def spectrum(n, field):
         q = field.order
         p = field.char
         d = gcd(n, q - e)
+
         # (1)
         eps = 1 if n % 2 == 0 else e
         a1 = [(q ** n - eps) // ((q - e) * d)]
+
         # (2)
         a2 = []
         eps = lambda s: 1 if s % 2 == 0 else e
@@ -433,8 +534,10 @@ def _projective_special_linear_spectrum(sign):
             signs = (-eps(n1), -eps(n - n1))
             a2.append(lcm(q ** pair[0] + signs[0], q ** pair[1] + signs[1]) //
                       gcd(n // gcd(n1, n - n1), q - e))
-            # (3)
+
+        # (3)
         a3 = SemisimpleElements(q, n, min_length=3, sign=e)
+
         # (4)
         a4 = []
         k = 1
@@ -445,9 +548,12 @@ def _projective_special_linear_spectrum(sign):
             eps = 1 if n1 % 2 == 0 else e
             a4.append(p ** k * (q ** n1 - eps) / d)
             k += 1
-            # (5)
-        a5 = MixedElements(q, n, lambda k: p ** (k - 1) + 1,
+
+        # (5)
+        a5 = MixedElements(q, n,
+                           lambda k: p ** (k - 1) + 1,
                            lambda k: p ** k, min_length=2, sign=e)
+
         # (6)
         k = numeric.get_exponent(n - 1, p)
         a6 = [] if k is None else [p * (n - 1)]
