c214a27c5fd2549761df14188d001fb509001b84
Daniel Lytkin <dan.lytkin@gmail.com>
2013-10-25 20:20:31 +0700

    added search box for apex list

--- src/spectrum/gui/gui_elements.py
+++ src/spectrum/gui/gui_elements.py
@@ -14,7 +14,7 @@ Copyright 2012 Daniel Lytkin.
    limitations under the License.
 
 """
-from Tkinter import Frame, Button, Listbox, Entry, StringVar, OptionMenu, Checkbutton, IntVar, Label, Menu, Scrollbar
+from Tkinter import Frame, Button, Listbox, Entry, StringVar, OptionMenu, Checkbutton, IntVar, Label, Menu, Scrollbar, LabelFrame
 import re
 import tkFont
 from spectrum.calculations.numeric import Integer, Constraints
@@ -34,22 +34,16 @@ class ApexList(Listbox):
         kw.setdefault('selectmode', 'extended')
         Listbox.__init__(self, parent, **kw)
         self.set_apex(apex)
-        self.bind("<Return>",
-            lambda event: self.expand_selected())
+        self.bind("<Return>", lambda event: self.expand_selected())
 
         self._init_menu()
 
         self._right_click = None
 
-
     def _init_menu(self):
         self._menu = Menu(self, tearoff=0)
         self._menu.add_command(label="Copy", command=self._copy_selected)
-        self._menu.add_command(label="Copy LaTeX",
-            command=self._copy_selected_latex)
-        self._menu.add_command(label="Copy all", command=self._copy_all)
-        self._menu.add_command(label="Copy all LaTeX",
-            command=self._copy_all_latex)
+        self._menu.add_command(label="Copy LaTeX", command=self._copy_selected_latex)
         self._menu.add_command(label="Expand", command=self.expand_selected)
 
         # right button on Mac and other systems
@@ -76,20 +70,16 @@ class ApexList(Listbox):
 
         pyperclip.setcb(", ".join(map(elem_latex, self.curselection())))
 
-    def _copy_all(self):
-        pyperclip.setcb(", ".join(map(str, self._apex)))
-
-    def _copy_all_latex(self):
-        def elem_latex(e):
-            if e.mode == StringViewFormatter.NORMAL:
-                return e.str_normal()
-            return e.str_latex()
-
-        pyperclip.setcb(", ".join(map(elem_latex, self._apex)))
-
     def curselection(self):
         """Return list of indices of currently selected items."""
         return [int(index) for index in Listbox.curselection(self)]
+
+    def select_by_divisor(self, divisor):
+        """Selects and entries divisible by 'divisor'."""
+        self.selection_clear(0, 'end')
+        for index, number in enumerate(self._apex):
+            if number.object % divisor == 0:
+                self.selection_set(index)
 
     def _update_text(self, index):
         self.delete(index)
@@ -150,11 +140,11 @@ class ApexListContainer(Frame):
         Frame.__init__(self, parent, **kw)
         self._init_components()
 
-
     def _init_components(self):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
+        # upper pane contains apex list
         upper_pane = Frame(self)
         upper_pane.columnconfigure(0, weight=1)
         upper_pane.rowconfigure(0, weight=1)
@@ -163,25 +153,55 @@ class ApexListContainer(Frame):
         self.apex_list.grid(row=0, column=0, sticky='nesw')
 
         self._scrollbar_x = Scrollbar(upper_pane, command=self.apex_list.xview,
-            orient='horizontal')
+                                      orient='horizontal')
         self._scrollbar_x.grid(row=1, column=0, sticky='ews')
         self.apex_list['xscrollcommand'] = self._scrollbar_x.set
         self._scrollbar_y = Scrollbar(upper_pane, command=self.apex_list.yview)
         self._scrollbar_y.grid(row=0, column=1, sticky='nse')
         self.apex_list['yscrollcommand'] = self._scrollbar_y.set
 
+        # panel with number search box
+        search_pane = LabelFrame(self, text="Find number")
+
+        self._search_box = NumberBox(search_pane, constraints=Constraints(min=1))
+        self._search_box.pack(side='left', expand=True, fill='x')
+
+        self._search_box_initial_bg = self._search_box['bg']
+        self._search_box_alert_bg = '#ffaaaa'
+
+        self._search_box.bind('<Return>', self._find_number)
+        for event in ('<FocusIn>', '<FocusOut>', '<Key>'):
+            self._search_box.bind(event, self._reset_search_box_alert)
+
+        search_number_button = Button(search_pane, text="Find", command=self._find_number)
+        search_number_button.pack()
+
+        # buttons pane contain
         buttons_pane = Frame(self)
 
-        self._expand_button = Button(buttons_pane, text="Expand all",
-            command=self.apex_list.expand_all)
+        self._expand_button = Button(buttons_pane, text="Expand All", command=self.apex_list.expand_all)
         self._expand_button.pack(side='left', expand=True, fill='x')
 
-        self._reset_button = Button(buttons_pane, text="Reset",
-            command=self.apex_list.reset)
+        self._reset_button = Button(buttons_pane, text="Collapse All", command=self.apex_list.reset)
         self._reset_button.pack()
 
         upper_pane.grid(sticky='nesw')
-        buttons_pane.grid(row=1, sticky='nesw')
+        search_pane.grid(row=1, sticky='nesw')
+        buttons_pane.grid(row=2, sticky='nesw')
+
+    def _find_number(self, *_):
+        self._reset_search_box_alert()
+
+        # select numbers divisible by input number
+        self._search_box.refresh_input()
+        self.apex_list.select_by_divisor(int(self._search_box.get()))
+
+        # if no numbers selected, paint background of search box to red
+        if not self.apex_list.curselection():
+            self._search_box['bg'] = self._search_box_alert_bg
+
+    def _reset_search_box_alert(self, *_):
+        self._search_box['bg'] = self._search_box_initial_bg
 
 
 class NumberBox(Entry):
@@ -209,7 +229,6 @@ class NumberBox(Entry):
         value = input
 
         value = self._constraints.closest_valid(value)
-
 
         #if input != value:
         # changed
@@ -295,7 +314,6 @@ class IntegerView(Label):
             cb = self._integer_view.str_normal()
         pyperclip.setcb(cb)
 
-
     def _update_width(self, event):
         self['wraplength'] = self.winfo_width() - 10
 
@@ -333,7 +351,7 @@ class IntegerContainer(Frame):
         self._integer_view.pack(expand=True, fill='x', side='left')
 
         self._button = CheckBox(self, indicatoron=0, text="F",
-            command=self._set_factorization)
+                                command=self._set_factorization)
         self._button.pack(side='left')
 
     def _set_factorization(self):
@@ -383,25 +401,10 @@ class FrameWithCloseButton(Frame):
         Frame.__init__(self, parent, **kw)
         self._init_button_area()
 
-
     def _init_button_area(self):
         self._button_area = Frame(self, relief='raised', bd=1)
         self._button_area.pack(expand=False, fill='x')
 
         self._close_button = Button(self._button_area, text='Close', bd=1,
-            command=self.destroy)
+                                    command=self.destroy)
         self._close_button.pack(side='right')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
