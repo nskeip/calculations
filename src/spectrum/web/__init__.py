992e7f37feabd65b35b398c0650334ec22a72d99
Nikita Hismatov <hismatovns@gmail.com>
2021-10-06 00:42:14 +0500

    wip: sporadic groups api

--- src/spectrum/web/__init__.py
+++ src/spectrum/web/__init__.py
@@ -14,18 +14,55 @@ Copyright 2021 Nikita Hismatov.
    limitations under the License.
 
 """
-from fastapi import FastAPI
+from enum import Enum
+from typing import List
 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, conlist
+
+from spectrum.calculations.graphs import PrimeGraph
 from spectrum.calculations.groups import SporadicGroup, ClassicalGroup, ExceptionalGroup
+
+
+class GroupType(Enum):  # TODO: probably should be used in all project
+    ALTERNATING = 'alternating'
+    CLASSICAL = 'classical'
+    EXCEPTIONAL = 'exceptional'
+    SPORADIC = 'sporadic'
+
+
+GROUP_TYPES = {
+    GroupType.ALTERNATING: [],
+    GroupType.CLASSICAL: ClassicalGroup.types(),
+    GroupType.EXCEPTIONAL: ExceptionalGroup.types(),
+    GroupType.SPORADIC: SporadicGroup.all_groups(),
+}
 
 app = FastAPI()
 
 
 @app.get('/groups/')
 async def groups():
+    return GROUP_TYPES
+
+
+# @app.get('/groups/{group_type}/{group_name}/')  # TODO: separate method for each type, separate Enums for group_names?
+# async def group_gk(group_type: GroupType, group_name: str):
+#     pass
+
+
+class GraphResponseModel(BaseModel):
+    vertices: List[int]
+    edges: List[conlist(item_type=int, min_items=2, max_items=2)]
+
+
+@app.get('/groups/sporadic/{group_name}/gk/', response_model=GraphResponseModel)
+async def sporadic_graph_gk(group_name: str):
+    if group_name not in GROUP_TYPES[GroupType.SPORADIC]:
+        raise HTTPException(status_code=404, detail="Item not found")
+    g = SporadicGroup(group_name)
+    graph = PrimeGraph(g)
     return {
-        'alternating': [],
-        'classical': ClassicalGroup.types(),
-        'exceptional': ExceptionalGroup.types(),
-        'sporadic': SporadicGroup.all_groups(),
+        'vertices': graph.vertices,
+        'edges': graph.edges,
     }
